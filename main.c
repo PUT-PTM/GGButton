1a3f49617e1efb5bc79dd535eef6f8d8722a0057
gaca1111 <gacqq@wp.pl>
2017-03-25 15:23:32 +0100

    Ustawianie pinu dla przerwania zewnetrznego
    szablon do tego dziala stabilnie 0 bledow (narazie)
    Q.Q

--- main.c
+++ main.c
@@ -3,12 +3,48 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_tim.h"
 #include "stm32f4xx_exti.h"
+#include "stm32f4xx_syscfg.h"
+#include "misc.h"
+#include "konfiguracja_przelacznika.h"
+#include "przerwania_zewnetrzne.h"
 
 
 int main(void)
 {
 	SystemInit();
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+	Przelacznik_konfiguracja();
+
+
+
+	// DEBOUNCER
+
+	/*
+		 * void EXTI0_IRQHandler ( void ){
+			if (EXTI_GetITStatus(EXTI_Line0) != RESET){
+				TIM_Cmd(TIM3, ENABLE);
+			}
+		}
+
+		void TIM3_IRQHandler ( void ){
+			if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET){
+				if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+
+
+					GPIO_ToggleBits(GPIOE, GPIO_Pin_7);
+					TIM_Cmd(TIM3, DISABLE);
+				TIM3->CNT = 0;
+			}
+
+			TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+			EXTI_ClearITPendingBit(EXTI_Line0);
+		}
+	}
+		 */
+
+
+
 
     while(1)
     {
--- przerwania_zewnetrzne.h
+++ przerwania_zewnetrzne.h
@@ -1,9 +1,17 @@
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
 #include "stm32f4xx_tim.h"
 #include "stm32f4xx_exti.h"
+#include "stm32f4xx_syscfg.h"
+#include "misc.h"
 
 #ifndef przerwanie
 #define przerwanie
 
+typedef enum port {A, B, C, D, E} port;
+
 void Ustaw_przerwanie_zewnetrzne(unsigned char przerwanie_czego);
+void Ustaw_pin_dla_przerwania_zewnetrznego(int linia_przerwania, unsigned int pin, port port_GPIO);
 
 #endif
