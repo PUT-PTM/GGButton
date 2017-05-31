254573686eea7053e002488ac34b13963257041e
gaca1111 <gacqq@wp.pl>
2017-05-31 20:54:11 +0200

    Koniec mam nadzieję
    +przerwanie odtwarzania po powtórnym wciśnięciu guzika
    +posprzątanie kodu

--- main.c
+++ main.c
@@ -12,43 +12,29 @@
 #include "misc.h"
 #include "delay.h"
 #include "codec.h"
-//#include "List.h"
 #include "ff.h"
-
 #include <stdbool.h>
-
 
 FATFS fatfs;
 FIL file;
 FRESULT fresult;
 DIR Dir;
 FILINFO fileInfo;
+UINT read_bytes;
 
 u16 sample_buffer[2048];
 
 volatile u16 result_of_conversion=0;
 
-volatile s8 change_song=0;
-volatile u8 error_state=0;
-
 const char* utwory[9] = {"0.WAV","1.WAV","2.WAV","3.WAV","4.WAV","5.WAV","6.WAV","7.WAV","8.WAV"};
 int obecny_utwor = 0;
 int graj = 0;
-int posladki = 0;
-int lel = 0;
-
 
 //===
 
 void EXTI9_5_IRQHandler ( void ){
 
 	if (EXTI_GetITStatus(EXTI_Line9) != RESET){
-
-		if(graj == 0){
-
-			graj = 1;
-		}
-
 
 		TIM_Cmd(TIM5, ENABLE);
 	}
@@ -62,9 +48,14 @@ void TIM5_IRQHandler ( void ){
 
 		if(GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_9)){
 
+			if(graj == 0){
 
-			graj = 1;
+						graj = 1;
+			}
+			else{
 
+				graj = 0;
+			}
 
 			GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
 
@@ -88,62 +79,6 @@ void TIM2_IRQHandler(void)
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 	}
 }
-
-
-
-
-
-
-/*
-void TIM4_IRQHandler(void)
-{
-	if(TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
-	{
-		if (error_state==1)// jesli uruchomiono program bez karty SD w module, zle podpieto kable
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-		}
-		if (error_state==2)// jesli wyjeto karte SD w trakcie odtwarzania plikow
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
-		}
-		if (error_state==3)// jesli na karcie SD nie ma plikow .wav
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
-		}
-
-		TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
-	}
-}
-*/
-
-
-/*
-void ERROR_TIM_4()
-{
-	// TIMER DO OBSLUGI DIOD W PRZYPADKU BLEDU
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-	TIM_TimeBaseInitTypeDef TIMER_4;
-
-	TIMER_4.TIM_Period = 24000-1;
-	TIMER_4.TIM_Prescaler = 1000-1;
-	TIMER_4.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIMER_4.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM4, &TIMER_4);
-	TIM_Cmd(TIM4,DISABLE);
-
-	// KONFIGURACJA PRZERWAN - TIMER/COUNTER
-	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;// numer przerwania
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;// priorytet glowny
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;// subpriorytet
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;// uruchom dany kanal
-	NVIC_Init(&NVIC_InitStructure);// zapisz wypelniona strukture do rejestrow
-	TIM_ClearITPendingBit(TIM4, TIM_IT_Update);// wyczyszczenie przerwania od timera 4 (wystapilo przy konfiguracji timera)
-	TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);// zezwolenie na przerwania od przepelnienia dla timera 4
-}
-*/
-
 
 //===
 
@@ -250,64 +185,75 @@ void Konfiguracja_diod()
 
 //===
 
-void MY_DMA_initM2P()
-{
+void MY_DMA_initM2P(){
+
 	DMA_InitTypeDef  DMA_InitStructure;
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
 	DMA_DeInit(DMA1_Stream5);
-	DMA_InitStructure.DMA_Channel = DMA_Channel_0;// wybor kanalu DMA
-	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;// ustalenie rodzaju transferu (memory2memory / peripheral2memory /memory2peripheral)
-	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;// tryb pracy - pojedynczy transfer badz powtarzany
-	DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;// ustalenie priorytetu danego kanalu DMA
-	DMA_InitStructure.DMA_BufferSize = 2048;// liczba danych do przeslania
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&sample_buffer;// adres zrodlowy
-	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(SPI3->DR));// adres docelowy
-	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;// zezwolenie na inkrementacje adresu po kazdej przeslanej paczce danych
+
+	DMA_InitStructure.DMA_Channel = DMA_Channel_0;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+	DMA_InitStructure.DMA_BufferSize = 2048;
+	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&sample_buffer;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(SPI3->DR));
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;// ustalenie rozmiaru przesylanych danych
-	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;// ustalenie trybu pracy - jednorazwe przeslanie danych
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
 	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
-	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;// wylaczenie kolejki FIFO
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
 	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
 
-	DMA_Init(DMA1_Stream5, &DMA_InitStructure);// zapisanie wypelnionej struktury do rejestrow wybranego polaczenia DMA
-	DMA_Cmd(DMA1_Stream5, ENABLE);// uruchomienie odpowiedniego polaczenia DMA
+	DMA_Init(DMA1_Stream5, &DMA_InitStructure);
+	DMA_Cmd(DMA1_Stream5, ENABLE);
 
 	SPI_I2S_DMACmd(SPI3,SPI_I2S_DMAReq_Tx,ENABLE);
 	SPI_Cmd(SPI3,ENABLE);
 }
-void ADC_conversion()
-{
-	// Odczyt wartosci przez odpytnie flagi zakonczenia konwersji
-	// Wielorazowe sprawdzenie wartosci wyniku konwersji
+
+//===
+
+void ADC_conversion(){
+
 	ADC_SoftwareStartConv(ADC1);
 	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
 	result_of_conversion = ((ADC_GetConversionValue(ADC1))/16);
 }
-void TIM2_ADC_init()
-{
-	// Wejscie do przerwania od TIM2 co <0.05 s
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	// 2. UTWORZENIE STRUKTURY KONFIGURACYJNEJ
-	TIM_TimeBaseInitTypeDef TIMER_2;
-	TIMER_2.TIM_Period = 2100-1;// okres zliczania nie przekroczyc 2^16!
-	TIMER_2.TIM_Prescaler = 2000-1;// wartosc preskalera, tutaj bardzo mala
-	TIMER_2.TIM_ClockDivision = TIM_CKD_DIV1;// dzielnik zegara
-	TIMER_2.TIM_CounterMode = TIM_CounterMode_Up;// kierunek zliczania
-	TIM_TimeBaseInit(TIM2, &TIMER_2);
-	TIM_Cmd(TIM2, ENABLE);// Uruchomienie Timera
 
-	// KONFIGURACJA PRZERWAN - TIMER/COUNTER
+//---
+
+void TIM2_ADC_init(){
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	TIM_TimeBaseInitTypeDef TIMER_2;
+	TIMER_2.TIM_Period = 2100-1;
+	TIMER_2.TIM_Prescaler = 2000-1;
+	TIMER_2.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIMER_2.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM2, &TIMER_2);
+
+	TIM_Cmd(TIM2, ENABLE);
+
+	//---
+
 	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;// numer przerwania
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;// priorytet glowny
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;// subpriorytet
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;// uruchom dany kanal
-	NVIC_Init(&NVIC_InitStructure);// zapisz wypelniona strukture do rejestrow
-	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);// wyczyszczenie przerwania od timera 2 (wystapilo przy konfiguracji timera)
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);// zezwolenie na przerwania od przepelnienia dla timera 2
+
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 }
+
+//---
+
 void ADC_init()
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA , ENABLE);
@@ -353,73 +299,60 @@ void ADC_init()
 	TIM2_ADC_init();
 }
 
+//===
 
-
-
-
-
-
-
-
-
-
-bool read_and_send(FRESULT fresult, int position, volatile ITStatus it_status, UINT read_bytes, uint32_t DMA_FLAG)
-{
-
+bool read_and_send(int position, volatile ITStatus it_status, uint32_t DMA_FLAG){
 
 	it_status = RESET;
-	while(it_status == RESET)
-	{
+	while(it_status == RESET){
+
 		it_status = DMA_GetFlagStatus(DMA1_Stream5, DMA_FLAG);
 	}
+
 	fresult = f_read (&file,&sample_buffer[position],1024*2,&read_bytes);
 	DMA_ClearFlag(DMA1_Stream5, DMA_FLAG);
 
-	if(fresult != FR_OK)// jesli wyjeto karte w trakcie odtwarzania plikow
-	{
-		error_state=2;
+	if(read_bytes<1024*2){
 
 		return 0;
 	}
-	if(read_bytes<1024*2||change_song!=0)
-	{
-		return 0;
-	}
+
 	return 1;
 }
 
-void play_wav(int piosenka)
-{
+//---
 
-	UINT read_bytes;// uzyta w f_read
-	fresult = f_open( &file, utwory[piosenka], FA_READ );
-	if( fresult == FR_OK )
-	{
-		fresult=f_lseek(&file,44);// pominiecie 44 B naglowka pliku .wav
-		volatile ITStatus it_status;// sprawdza flage DMA
-		change_song=0;
+void play_wav(int piosenka) {
 
+	fresult = f_open(&file,utwory[piosenka],FA_READ);
+
+	if( fresult == FR_OK ){
+
+		fresult=f_lseek(&file,44);
+		volatile ITStatus it_status;
 
 		while(1)
 		{
-			if (read_and_send(fresult,0, it_status, read_bytes, DMA_FLAG_HTIF5)==0)
+			if (read_and_send(0, it_status, DMA_FLAG_HTIF5)==0)
 			{
 				break;
 			}
-			if (read_and_send(fresult, 1024, it_status, read_bytes, DMA_FLAG_TCIF5)==0)
+			if (read_and_send(1024, it_status, DMA_FLAG_TCIF5)==0)
 			{
 				break;
+			}
+
+			if(graj == 0){
+
+				return;
 			}
 		}
-
-		GPIO_SetBits(GPIOD, GPIO_Pin_13);
-
-		Codec_VolumeCtrl(0);
 
 		fresult = f_close(&file);
 	}
 }
 
+//===
 
 int main( void )
 {
@@ -428,54 +361,20 @@ int main( void )
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 	RCC_AHB2PeriphClockCmd(RCC_AHB2Periph_RNG, ENABLE);
 
-
-	delay_init( 80 );// wyslanie 80 impulsow zegarowych; do inicjalizacji SPI
-	SPI_SD_Init();// inicjalizacja SPI pod SD
-
-
-
-
-	// SysTick_CLK... >> taktowany z f = ok. 82MHz/8 = ok. 10MHz
-	// Systick_Config >> generuje przerwanie co <10ms
-	//SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);// zegar 24-bitowy
-	//SysTick_Config(90000);
-
-	// SD CARD
-
-
-
-
-
-	disk_initialize(0);// inicjalizacja karty
-	fresult = f_mount( &fatfs, 1,1 );// zarejestrowanie dysku logicznego w systemie
-
-
-
+	delay_init( 80 );
+	SPI_SD_Init();
+	disk_initialize(0);
+	fresult = f_mount(&fatfs,1,1);
 	codec_init();
 	codec_ctrl_init();
-	I2S_Cmd(CODEC_I2S, ENABLE);// Integrated Interchip Sound to connect digital devices
+	I2S_Cmd(CODEC_I2S, ENABLE);
 	MY_DMA_initM2P();
-
 	ADC_init();
-
 	Konfiguracja_diod();
-
 	Konfiguracja_przyciskow();
 	Konfiguracja_debouncera();
 
-
-
-
-
-
-
-//	RNG_Cmd(ENABLE);
-//	u32 rand_number=0;
-	//u32 i_loop=0;
-	//GPIO_SetBits(GPIOD, GPIO_Pin_14);
-
-
-
+	//---
 
 	for(;;)
 	{
@@ -526,8 +425,6 @@ int main( void )
 
 			graj = 0;
 		}
-
-		posladki++;
 	}
 
 	return 0;
